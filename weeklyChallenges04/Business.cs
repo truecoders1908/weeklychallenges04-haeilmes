d8aac314f3fadd8113f2dd077ad445402f069261
Aziza Hurt <imazizahurt@gmail.com>
2019-09-16 13:59:13 -0500

    initial commit

---
+++ weeklyChallenges04/Business.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace weeklyChallenges04
+{
+    
+		public class Business
+		{
+			public string Name { get; set; }
+			public double TotalRevenue { get; set; }
+			public double TotalExpenses { get; set; }
+		}
+	
+}
