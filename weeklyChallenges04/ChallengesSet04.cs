679a5d4c7b2ed528f602c793013e063805128819
Hunter <haeilmes@gmail.com>
2019-09-17 10:08:38 -0500

    First Commit

--- weeklyChallenges04/ChallengesSet04.cs
+++ weeklyChallenges04/ChallengesSet04.cs
@@ -1,26 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace weeklyChallenges04
 {
     public class ChallengesSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int numTotal = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    numTotal += num;
+                }
+                else
+                {
+                    numTotal -= num;
+                }
+            }
+            return numTotal;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<int> intList = new List<int>();
+            intList.Add(str1.Length);
+            intList.Add(str2.Length);
+            intList.Add(str3.Length);
+            intList.Add(str4.Length);
+
+            return intList.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> intList = new List<int>();
+            intList.Add(number1);
+            intList.Add(number2);
+            intList.Add(number3);
+            intList.Add(number4);
+
+            return intList.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
@@ -30,7 +57,7 @@ namespace weeklyChallenges04
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return input.All(char.IsNumber);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
